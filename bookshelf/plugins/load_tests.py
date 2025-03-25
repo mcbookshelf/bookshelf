6c313e714cf11a47bef88ac936a78d53f48c924c
Aksiome <54895777+aksiome@users.noreply.github.com>
2025-03-26 00:36:42 +0100

    random fixes

--- bookshelf/plugins/load_tests.py
+++ bookshelf/plugins/load_tests.py
@@ -23,20 +23,29 @@ def beet_default(ctx: Context) -> Generator:
     ctx.data.extend_namespace.append(TestEnvironment)
     ctx.data.extend_namespace.append(TestFunction)
     yield
+
     env = f"# @environment {ctx.directory.name}:default\n"
     header = ctx.template.render("bookshelf/header.jinja")
     offset = len(header)
+    count = 0
 
     for _, file in ctx.data.all(extend=TestFunction):
+        count += 1
         file.set_content(f"{file.text[:offset]}{env}{file.text[offset:]}")
 
-    ctx.data[f"{ctx.directory.name}:__test__"] = Function([
-        header,
-        "function #bs.load:unload",
-        f"function #{ctx.directory.name}:load",
-    ])
+    if count > 0:
+        ctx.data[f"bs.load:test/{ctx.directory.name}/__setup__"] = Function([
+            header,
+            f"function #{ctx.directory.name}:load",
+        ])
 
-    ctx.data[f"{ctx.directory.name}:default"] = TestEnvironment({
-        "type": "minecraft:function",
-        "setup": f"{ctx.directory.name}:__test__",
-    })
+        ctx.data[f"bs.load:test/{ctx.directory.name}/__teardown__"] = Function([
+            header,
+            f"function #{ctx.directory.name}:unload",
+        ])
+
+        ctx.data[f"{ctx.directory.name}:default"] = TestEnvironment({
+            "type": "minecraft:function",
+            "setup": f"bs.load:test/{ctx.directory.name}/__setup__",
+            "teardown": f"bs.load:test/{ctx.directory.name}/__teardown__",
+        })
