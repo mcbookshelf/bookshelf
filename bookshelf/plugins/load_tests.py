5001487515135532a422b7f070703c7d57927814
Aksiome <54895777+aksiome@users.noreply.github.com>
2025-03-25 19:36:49 +0100

    wip

--- bookshelf/plugins/load_tests.py
+++ bookshelf/plugins/load_tests.py
@@ -1,7 +1,14 @@
 from collections.abc import Generator
 from typing import ClassVar
 
-from beet import Context, NamespaceFileScope, TextFile
+from beet import Context, Function, JsonFile, NamespaceFileScope, TextFile
+
+
+class TestEnvironment(JsonFile):
+    """Represents a Minecraft Test Environment file."""
+
+    scope: ClassVar[NamespaceFileScope] = ("test_environment",)
+    extension: ClassVar[str] = ".json"
 
 
 class TestFunction(TextFile):
@@ -13,18 +20,23 @@ class TestFunction(TextFile):
 
 def beet_default(ctx: Context) -> Generator:
     """Include test functions from the test folder."""
+    ctx.data.extend_namespace.append(TestEnvironment)
     ctx.data.extend_namespace.append(TestFunction)
     yield
-    batch = f"# @batch {ctx.directory.name}\n"
+    env = f"# @environment {ctx.directory.name}:default\n"
     header = ctx.template.render("bookshelf/header.jinja")
     offset = len(header)
 
     for _, file in ctx.data.all(extend=TestFunction):
-        file.set_content(f"{file.text[:offset]}{batch}{file.text[offset:]}")
+        file.set_content(f"{file.text[:offset]}{env}{file.text[offset:]}")
 
-    with ctx.override(generate_namespace=ctx.directory.name):
-        ctx.generate("__setup__", TestFunction(
-            f"{header}{batch}"
-            f"# @beforebatch "
-            f'function #bs.load:exclusive {{module:"{ctx.directory.name}"}}',
-        ))
+    ctx.data[f"{ctx.directory.name}:__test__"] = Function([
+        header,
+        "function #bs.load:unload",
+        f"function #{ctx.directory.name}:load",
+    ])
+
+    ctx.data[f"{ctx.directory.name}:default"] = TestEnvironment({
+        "type": "minecraft:function",
+        "setup": f"{ctx.directory.name}:__test__",
+    })
